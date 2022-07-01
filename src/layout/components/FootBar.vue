0a991e8cd3b130ae307aeafc96536ca4fe8902a9
lhywell <lihuiyin128@163.com>
2022-07-01 14:55:46 +0800

    更新公安备案

--- src/layout/components/FootBar.vue
+++ src/layout/components/FootBar.vue
@@ -1,6 +1,15 @@
 <template>
   <el-footer class="footer">
-    <span>© {{ year }} guanyan001.cn 版权所有 增值电信业务经营许可证： </span>
+    <span>© {{ year }} guanyan001.cn 版权所有</span>
+    <div style="display: inline-block; line-height: 60px; margin: 0 20px">
+      <a
+        target="_blank"
+        href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11011202003676"
+        class="text-color gongan"
+      >
+        <img src="gongan.png" />&nbsp;京公网安备 11011202003676号
+      </a>
+    </div>
     <a class="text-color" href="https://beian.miit.gov.cn" target="_blank">京ICP备2022016336号</a>
   </el-footer>
 </template>
@@ -26,12 +35,20 @@ export default {
   line-height: 60px;
   background: rgb(37, 37, 37);
   color: #73777a;
+  font-size: 13px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   a {
     color: #73777a;
   }
   .text-color:hover {
     color: #c8cacc;
+  }
+  .gongan {
+    display: flex;
+    align-items: center;
   }
 }
 </style>
